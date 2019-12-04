0a32c5716048632c58d032fdcc10ee5b51945430
Minna Roberto <minnaroberto14@yahoo.ca>
2019-12-04 17:28:47 -0600

    Add magnet sensor and led code
    When a magnet sensor comes in contact with a magnet an led will light up

--- Arduino_Maze/Arduino_Maze.ino
+++ Arduino_Maze/Arduino_Maze.ino
@@ -1,8 +1,8 @@
 /*
  * Minna Roberto
- * Arduino Maze Project
- * test check
- */
+ * Arduino Maze with Magnetic Contact Sensors Project
+ * December 6, 2019
+*/
 
 #include <Servo.h>
 
@@ -11,19 +11,36 @@ Servo servo2;
 
 int joyX = 1;
 int joyY = 0;
-
 int joyVal;
 
+const int led1 = 1;
+const int led2 = 2; 
+const int led2 = 4; 
+
+const int sensor1 = 6;
+const int sensor2 = 7;
+const int sensor3 = 8; 
+
+//checkpoints: check whether a magnet sensor is closed or not
+int checkpoint1; 
+int checkpoint2; 
+int checkpoint3; 
+
 void setup() {
-  // put your setup code here, to run once:
+
   servo1.attach(3);
   servo2.attach(5);
-  Serial.begin(9600);
+
+  pinMode(sensor1, INPUT_PULLUP);
+  pinMode(sensor2, INPUT_PULLUP);
+  pinMode(sensor3, INPUT_PULLUP);
+  
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly
-
     joyVal = analogRead(joyX);
     joyVal = map(joyVal, 0, 1023, 0, 179);
     servo1.write(joyVal);
@@ -32,5 +49,35 @@ void loop() {
     joyVal = map(joyVal, 0, 1023, 0, 179);
     servo2.write(joyVal);
   
-      delay(15);
+    delay(15);
+
+
+    checkpoint1 = digitalRead(sensor1); 
+    checkpoint2 = digitalRead(sensor2);
+    checkpoint3 = digitalRead(sensor3);
+
+    //check if a magnet sensor is in contact with a magnet
+    if (checkpoint1 == LOW){
+     digitalWrite(led1, HIGH); //turn an led on
+    }
+    else{
+      digitalWrite(led1, LOW); //led is off
+    }
+  
+    //check if a magnet sensor is in contact with a magnet
+    if (checkpoint2 == LOW){
+      digitalWrite(led2, HIGH);
+    }
+    else{
+     digitalWrite(led2, LOW);
+    }
+  
+    //check if a magnet sensor is in contact with a magnet
+    if (checkpoint3 == LOW){
+      digitalWrite(led3, HIGH);
+    }
+    else{
+      digitalWrite(led3, LOW);
+    }
+    delay(200);
 }
